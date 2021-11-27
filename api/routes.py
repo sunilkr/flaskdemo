0ec610d424abc5aa0f88d7e1bffab366ab12e795
Sunil Kumar <badboy16a@gmail.com>
2021-11-27 19:00:41 +0000

    Added books model.

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,7 @@
+from flask import json
 from flask.json import jsonify
-from flask import Blueprint
+from flask import Blueprint, request
+from ..models.book import Book
 
 
 api_bp = Blueprint("api", __name__)
@@ -12,3 +14,26 @@ def hello():
 def get_user(id):
     #user = User(id=1, username='test', email='test@email.com')
     return jsonify(None)
+
+@api_bp.route("/book/<id>")
+def get_book(id):
+    book = Book()
+    book.id = int(id)
+    book.name = "Its a new book"
+    book.isbn = "1"*13
+    return jsonify(book.serialize())
+
+@api_bp.route("/book/new", methods=["POST"])
+def new_book():
+    req_data = json.loads(request.data)
+    book = Book()
+    book.id = int(req_data['id'])
+    book.isbn = req_data['isbn']
+    book.name = req_data['name']
+    book.save()
+    return jsonify(req_data)
+
+@api_bp.route("/books", methods=["GET"])
+def get_books():
+    books = Book.query.all()
+    return jsonify(map(lambda b: b.serialize(), books))
--- app.py
+++ app.py
@@ -9,9 +9,18 @@ app = create_app()
 app.register_blueprint(ui_bp, url_prefix="/ui")
 app.register_blueprint(api_bp, url_prefix="/api")
 db.init_app(app)
+
 with app.app_context():
     import routes
+    from .models.book import Book
+    
+    #db.init_app(app=app)
     db.create_all()
+    db.session.commit()
+    #db.session.add(Book())
+    #db.session.commit()
+    #books = Book.query.all()
+    #print(books)
 
 print (app.config['DEBUG'])
 print (basedir)
